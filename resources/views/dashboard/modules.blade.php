d31b424e52f00af4888995adc13aeb0b734e7865
Shamim Haque <shamim.haque.dev@gmail.com>
2022-07-21 17:33:18 +0600

    Dashboard Design Update

--- resources/views/dashboard/modules.blade.php
+++ resources/views/dashboard/modules.blade.php
@@ -28,33 +28,28 @@
                     $fontLine = '<h1><i class="las la-school font-large-2" title="BTC"></i></h1>';
             }
         @endphp
-        <div class="col-xl-3 col-12 text-center">
-            <div class="card crypto-card-3 pull-up">
-                <div class="card-content" style="{{ $style }}">
-                    <div class="card-body module-wrap pb-0 pt-0">
-                        {{-- <div class="row"> --}}
-                        {{-- <div class="col-2"> --}}
-                        {{-- <i class="las la-school"></i> --}}
-                        {{-- <h1><i class="ft-grid warning font-large-2" title="BTC"></i></h1> --}}
-                        {{-- <h1><i class="las la-school font-large-2" title="BTC"></i></h1> --}}
-                        {!! $fontLine !!}
-                        {{-- </div> --}}
-                        {{-- <div class="col-8 pl-2"> --}}
-                        
-                        @php
-                            $name = 'name_'.$lang;
-                        @endphp
-                        <h4>{{ $module->$name }}</h4>
-                        <h6 class="text-muted"><a
-                                href="{{ url(config('app.admin_prefix').'/'.strtolower($module->slug)) . '/' }}">{{ $module->$name }}
-                                <i class="las la-angle-double-right" style="color:#ffffff !important"></i></a>
-                        </h6>
-                        {{-- </div> --}}
-                        {{-- </div> --}}
+
+        @php
+            $name = 'name_'.$lang;
+        @endphp
+        <div class="col-xl-4 col-12 text-center">
+            <a href="{{ url(config('app.admin_prefix').'/'.strtolower($module->slug)) . '/' }}" class="card crypto-card-3 pull-up">
+                <div class="card-content">
+                    <div class="card-body module-wrap pb-0 pt-0 d-flex dashboard-box">
+                        <div class="icon" style="{{ $style }}">
+                            {!! $fontLine !!}
+                        </div>
+                        <div class="info">
+                            <h4>{{ $module->$name }}</h4>
+                            <h6 class="text-muted">
+                                {{ $module->$name }}
+                            </h6>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </a>
         </div>
+
         {{-- @endcan --}}
     @endforeach
     <div class="col-xl-4 col-12" style="display: none;">
